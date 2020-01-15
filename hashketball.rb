b84c7aee1cbfe22cafab43d974042efcdd15dd09
Jack Overby <jroverby92@gmail.com>
2020-01-15 18:57:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -195,4 +195,4 @@ def player_stats(player_name)
     }.select{ |k, v| k.to_s != "player_name"}
 end
 
-game_hash.map{ |k, v| v[:players]}.flatten.max_by{ |player| player :shoe] }
+game_hash.map{ |k, v| v[:players]}.flatten.max_by{ |player| player[:shoe] }

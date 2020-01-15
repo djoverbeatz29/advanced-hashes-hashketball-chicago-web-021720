e56812162b0c14ce7bff3cb01e27474f82c9aada
Jack Overby <jroverby92@gmail.com>
2020-01-15 18:47:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -189,10 +189,10 @@ def player_numbers(team_name)
 end
 
 def player_stats(player_name)
-  ho = game_hash.map{ |k, v|
+  game_hash.map{ |k, v|
     v[:players].find { |player|
       player[:player_name] == player_name
     }
   }[0]
-  ho.select{ |k, v| k.to_s != "player_name"}
+  # ho.select{ |k, v| k.to_s != "player_name"}
 end

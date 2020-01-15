e4db10b1078f133f4e360c1a4d994f1e58f2e6f3
Jack Overby <jroverby92@gmail.com>
2020-01-15 19:23:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -206,5 +206,5 @@ end
 def winning_team
   names = game_hash.map{ |k, v| v[:team_name] }
   total_scores = game_hash.map{ |k, v| v[:players].sum{ |p| p[:points] }}
-  names[total_scores.max_with_index[max]]
+  names[total_scores.max_with_index.max[1]]
 end
